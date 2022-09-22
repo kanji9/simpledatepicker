891f05ec0b999f31d50b9c244cde3550e6fc1596
carmine92 <carmine.992@gmail.com>
2022-09-22 23:51:42 +0200

    printing days

--- picker/simpledatepicker.js
+++ picker/simpledatepicker.js
@@ -1,5 +1,25 @@
 class SimpleDatePicker{
     init(){
         this.cDayTbl = document.querySelector ("#simplepicker____days table tbody");
+
+        
+        console.table(Utility.getMonthDays());
     }
 }
+
+class Utility{
+    static getMonthDays(year, month) {
+        if(!year)
+            year = new Date().getFullYear();
+        if(!month)
+            month = new Date().getMonth();
+        const date = new Date(year, month, 1);
+        const days = [];
+        while (date.getMonth() == month) {
+            days.push(new Date(date));
+            date.setDate(date.getDate() + 1);
+        }
+        return days;
+    }
+}
+

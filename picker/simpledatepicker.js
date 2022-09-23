eb1631d8aba59377e31999323b30bc4d7ed8136a
carmine92 <carmine.992@gmail.com>
2022-09-24 01:01:38 +0200

    generate years and month, init table via template

--- picker/simpledatepicker.js
+++ picker/simpledatepicker.js
@@ -11,12 +11,68 @@ class SimpleDatePicker{
     }
 
     init(){
+        document.getElementById("selector").innerHTML = this.generateHtml();
         this.DayTbl = document.querySelector ("#simplepicker____days table tbody");
         this.printTable(this.DayTbl);
         document.addEventListener('click', (e)=>{
             if(e.target && e.target.classList.contains(AVAILABLE_CLASSNAME))
             this.OnSelect(e.target.getAttribute(DATA_ATTRIBUTE));
         });
+    }
+
+    generateHtml(){
+        let cTable = `
+            <div id="____simplepicker">
+                <div id="simplepicker____top_controls">
+                    <select id="simplepicker____months">
+                        ${this.printMonths().join("<br/>")}
+                    </select>
+                    <select id="simplepicker____years">
+                        ${this.printYears().join("<br/>")}
+                    </select>
+                </div>
+                <div id="simplepicker____days">
+                    <table >
+                        <thead>
+                            <tr>
+                                <th>Sun</th>
+                                <th>Mon</th>
+                                <th>Tue</th>
+                                <th>Wed</th>
+                                <th>Thu</th>
+                                <th>Fri</th>
+                                <th>Sat</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                    <div id="simplepicker____footer">
+                        <button>Ok</button>
+                        <button>Cancel</button>
+                    </div>
+                </div>
+            </div>
+        `;
+        return cTable;
+    }
+
+    printMonths(){
+        let cMonths = ["Gen", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        let cMonthOpt = new Array();
+        cMonths.forEach((e,i)=> { cMonthOpt.push(`<option value="${i}">${e}</option>`); });
+        return cMonthOpt;
+    }
+
+    printYears(){
+        let nStart = 1900;
+        let cYears = new Array();
+        for(let i=0;i<200;i++)
+            cYears.push(nStart++);
+        let cYearsOpt = new Array();
+        cYears.forEach(e=> cYearsOpt.push(`<option value="${e}">${e}</option>`));
+        return cYearsOpt;
     }
 
     printTable(ctbl, cYear, cMonth){
@@ -58,7 +114,6 @@ class SimpleDatePicker{
             }
             ctbl.appendChild(cRow);
         }
-
     }
 }
 
